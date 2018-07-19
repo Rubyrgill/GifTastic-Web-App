5c411ee5bf15da08cafb553aa56aca2c64126ed3
RubyG <rubyrgill@hotmail.com>
2018-07-18 21:48:01 -0400

    button function added, checked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,15 +4,45 @@ $(document).ready(function () {
     //_____________________________________________
     var sweetsOptions = ["Cake", "Berries", "Donuts", "Milkshakes"];
 
+    //FUNCTIONS
+    //_______________________________________________
+
+    //Creating a Button for sweets array 
+    function createButton() {
+        //clear
+        $("#button").empty();
+
+        //loop through array, and append to html
+        for (var i = 0; i < sweetsOptions.length; i++) {
+            var sOption = $("<button>")
+            sOption.addClass("treat");
+            sOption.attr("data-name", sweetsOptions[i]);
+            sOption.text(sweetsOptions[i]);
+            $("#button").append(sOption);
+        }
+    }
+
+
+    //calling function to check 
+    createButton()
+
+
+
+
+
+
+
+
+
     //storing url values in var 
     var inputOption = $(this).attr("data-input");
     var apiKey = "WDYt2fJ2U5xULEOYfSUWcwiDpRWR0EyX";
     var limitOf = 10;
-    var fullUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + inputOption + "&limit=" + limitOf + "&offset=0&rating=PG&lang=en";
+    var fullUrl = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&q=" + inputOption + "&limit=" + limitOf + "&offset=0&rating=G&lang=en";
 
 
 
-    //FUNCTIONS
+
     $.ajax({
         url: fullUrl,
         method: 'GET'
@@ -35,3 +65,6 @@ $(document).ready(function () {
 
 
 })
+
+
+
